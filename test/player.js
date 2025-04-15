4f5b01dc904422fa140257f84f4e610e53a956ff
ian.liu <ian.liu@whaletv.com>
2025-04-15 11:00:05 +0800

    ✨ feat:mse indexedDB 测试

--- test/player.js
+++ test/player.js
@@ -1,3 +1,185 @@
+// db-promise.js
+function IDBCache(dbName, storeName) {
+  this.dbName = dbName || 'http-cache';
+  this.storeName = storeName || 'responses';
+  this.db = null;
+}
+
+// 打开/初始化数据库
+IDBCache.prototype.open = function () {
+  var self = this;
+  return new Promise(function (resolve, reject) {
+    var request = indexedDB.open(self.dbName, 1);
+
+    request.onupgradeneeded = function (event) {
+      var db = event.target.result;
+      if (!db.objectStoreNames.contains(self.storeName)) {
+        db.createObjectStore(self.storeName, { keyPath: 'url' });
+      }
+    };
+
+    request.onsuccess = function (event) {
+      self.db = event.target.result;
+      resolve(self.db);
+    };
+
+    request.onerror = function (event) {
+      reject('IndexedDB打开失败: ' + event.target.error);
+    };
+  });
+};
+
+// 存储响应
+IDBCache.prototype.put = function (url, response) {
+  var self = this;
+
+  return new Promise(function (resolve, reject) {
+    // 确保数据库已打开
+    var openPromise = self.db ? Promise.resolve(self.db) : self.open();
+
+    openPromise
+      .then(function () {
+        return self._responseToStorable(response);
+      })
+      .then(function (storableData) {
+        return new Promise(function (innerResolve, innerReject) {
+          var transaction = self.db.transaction(self.storeName, 'readwrite');
+          var store = transaction.objectStore(self.storeName);
+
+          var request = store.put(Object.assign({ url: url }, storableData));
+
+          request.onsuccess = function () {
+            innerResolve();
+          };
+          request.onerror = function (event) {
+            innerReject(event.target.error);
+          };
+        });
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+};
+
+// 获取响应
+IDBCache.prototype.match = function (url) {
+  var self = this;
+
+  return new Promise(function (resolve, reject) {
+    var openPromise = self.db ? Promise.resolve(self.db) : self.open();
+
+    openPromise
+      .then(function () {
+        return new Promise(function (innerResolve, innerReject) {
+          var transaction = self.db.transaction(self.storeName, 'readonly');
+          var store = transaction.objectStore(self.storeName);
+          var request = store.get(url);
+
+          request.onsuccess = function (event) {
+            innerResolve(event.target.result);
+          };
+          request.onerror = function (event) {
+            innerReject(event.target.error);
+          };
+        });
+      })
+      .then(function (data) {
+        if (!data) {
+          resolve(undefined);
+          return;
+        }
+        return self._storableToResponse(data).then(resolve);
+      })
+      .catch(reject);
+  });
+};
+
+// 删除缓存项
+IDBCache.prototype.delete = function (url) {
+  var self = this;
+
+  return new Promise(function (resolve, reject) {
+    var openPromise = self.db ? Promise.resolve(self.db) : self.open();
+
+    openPromise
+      .then(function () {
+        return new Promise(function (innerResolve, innerReject) {
+          var transaction = self.db.transaction(self.storeName, 'readwrite');
+          var store = transaction.objectStore(self.storeName);
+          var request = store.delete(url);
+
+          request.onsuccess = function () {
+            innerResolve();
+          };
+          request.onerror = function (event) {
+            innerReject(event.target.error);
+          };
+        });
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+};
+
+// 清空缓存
+IDBCache.prototype.clear = function () {
+  var self = this;
+
+  return new Promise(function (resolve, reject) {
+    var openPromise = self.db ? Promise.resolve(self.db) : self.open();
+
+    openPromise
+      .then(function () {
+        return new Promise(function (innerResolve, innerReject) {
+          var transaction = self.db.transaction(self.storeName, 'readwrite');
+          var store = transaction.objectStore(self.storeName);
+          var request = store.clear();
+
+          request.onsuccess = function () {
+            innerResolve();
+          };
+          request.onerror = function (event) {
+            innerReject(event.target.error);
+          };
+        });
+      })
+      .then(resolve)
+      .catch(reject);
+  });
+};
+
+// 将Response对象转换为可存储格式
+IDBCache.prototype._responseToStorable = function (response) {
+  var headers = {};
+  response.headers.forEach(function (value, key) {
+    headers[key] = value;
+  });
+
+  return response.arrayBuffer().then(function (body) {
+    return {
+      status: response.status,
+      statusText: response.statusText,
+      headers: headers,
+      body: body,
+      timestamp: Date.now(),
+    };
+  });
+};
+
+// 将存储的数据转换回Response对象
+IDBCache.prototype._storableToResponse = function (data) {
+  return Promise.resolve(
+    new Response(data.body, {
+      status: data.status,
+      statusText: data.statusText,
+      headers: data.headers,
+    })
+  );
+};
+
+var CACHE_NAME = 'app-cache-v1';
+var idbCache = new IDBCache(CACHE_NAME);
+
 class MultiVideoPlayer {
   constructor() {
     this.videoElement = document.getElementById('videoPlayer');
@@ -5,12 +187,12 @@ class MultiVideoPlayer {
     this.videoSegments = [
       {
         id: 'part1',
-        url: 'https://assets.springserve.com/video_creatives/000/534/305/1241179_1280x720-4482558.mp4',
+        url: 'https://assets.springserve.com/video_creatives/000/606/368/15s-naturesacpe-5001500.mp4',
         preload: true,
       },
       {
         id: 'part2',
-        url: 'https://assets.springserve.com/video_creatives/000/606/368/15s-naturesacpe-5001500.mp4',
+        url: 'https://assets.springserve.com/video_creatives/000/534/305/1241179_1280x720-4482558.mp4',
         preload: true,
       },
       {
@@ -83,25 +265,37 @@ class MultiVideoPlayer {
 
   getCachedVideoUrl(videoUrl) {
     return new Promise((resolve) => {
-      if (!('caches' in window)) resolve(null);
+      // if (!('caches' in window)) resolve(null);
 
-      try {
-        caches.open('video-cache').then((cache) => {
-          cache.match(videoUrl).then((response) => {
-            console.log(1111, videoUrl, response);
-            if (response) {
-              response.blob().then((blob) => {
-                resolve(URL.createObjectURL(blob));
-              });
-            } else {
-              resolve(null);
-            }
+      // try {
+      //   caches.open('video-cache').then((cache) => {
+      //     cache.match(videoUrl).then((response) => {
+      //       console.log(1111, videoUrl, response);
+      //       if (response) {
+      //         response.blob().then((blob) => {
+      //           resolve(URL.createObjectURL(blob));
+      //         });
+      //       } else {
+      //         resolve(null);
+      //       }
+      //     });
+      //   });
+      // } catch (error) {
+      //   console.error('缓存访问失败:', error);
+      //   resolve(null);
+      // }
+      idbCache.match(videoUrl).then(function (cachedResponse) {
+        // 如果缓存中有且未过期，直接返回
+        if (cachedResponse) {
+          cachedResponse.blob().then((blob) => {
+            const blobUrl = URL.createObjectURL(blob);
+            console.log(991199, blobUrl);
+            resolve(blobUrl);
           });
-        });
-      } catch (error) {
-        console.error('缓存访问失败:', error);
-        resolve(null);
-      }
+        } else {
+          resolve(null);
+        }
+      });
     });
   }
 
@@ -124,38 +318,32 @@ class MultiVideoPlayer {
       nextIndex,
       nextIndex + 2
     );
-    console.log(2222);
     for (const segment of segmentsToPrefetch) {
       if (!segment.preload || this.prefetchedSegments.has(segment.url))
         continue;
 
-      try {
-        // 使用fetch API获取并缓存
-        fetch(segment.url, { mode: 'cors' }).then((response) => {
+      // 使用fetch API获取并缓存
+      fetch(segment.url, { mode: 'cors' })
+        .then((response) => {
           console.log(22245, response.ok);
           const clonedResponse = response.clone();
           if (response.ok) {
-            if ('caches' in window) {
-              caches.open('video-cache').then((cache) => {
-                console.log(999, clonedResponse, cache.put);
-                cache.delete(segment.url).then(() => {
-                  cache.put(segment.url, clonedResponse).catch((error) => {
-                    console.error('PUT 失败:', error);
-                  });
-                });
-                // cache.put(segment.url, clonedResponse).catch(error => {
-                //   console.error("PUT 失败:", error);
-                // });
-                // cache.put(segment.url, response.clone()).then(() => {
-                //   this.prefetchedSegments.set(segment.url, true);
-                // });
-              });
-            }
+            idbCache.put(segment.url, clonedResponse).catch(function (err) {
+              console.error('缓存失败:', err);
+            });
+            // if ('caches' in window) {
+            //   caches.open('video-cache').then((cache) => {
+            //     cache.put(segment.url, clonedResponse).catch((error) => {
+            //       console.error('PUT 失败:', error);
+            //     });
+            //   });
+
+            // }
           }
+        })
+        .catch((error) => {
+          console.error('预加载失败:', segment.url, error);
         });
-      } catch (error) {
-        console.error('预加载失败:', segment.url, error);
-      }
     }
   }
 
